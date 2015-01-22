b26be2e755c504186cee834d012b7cb12cd4d6ab
rotmankl <kraig>
2015-01-21 20:47:01 -0500

    RotBot Direct Configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -27,7 +27,7 @@
 #define STRING_VERSION "v1.0.2"
 #define STRING_URL "RotBot"
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "(RotBot Bowden)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(RotBot Direct)" // Who made the changes.
 #define STRING_SPLASH STRING_VERSION " - " STRING_URL // will be shown during bootup
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
